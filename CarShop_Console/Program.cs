5170cfb21c4ae5ef536c2b34069c1a68ee7f0289
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-01-19 09:45:01 +0100

    Better main console program

--- CarShop_Console/Program.cs
+++ CarShop_Console/Program.cs
@@ -9,27 +9,73 @@ namespace CarShop_Console
 {
     internal class Program
     {
+        static List<Veicolo> ParcoMezzi = new List<Veicolo>();
+
         static void Main(string[] args)
         {
-            // string myName = "Oscar";
-            // Console.WriteLine(ClassTest.HelloWorld(myName));
+            CreaDatiDiProva();
+            char scelta = ' ';
+            while (scelta != 'q' && scelta != 'Q')
+            {
+                scelta = ScriviMenu();
+                switch (scelta)
+                {
+                    case '1':
+                        ElencoVeicoli();
+                        break;
+                    case '2':
+                        ElencoAuto();
+                        break;
+                    case '3':
+                        ElencoMoto();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
 
-            Veicolo v = new Auto("BMW", "Serie 3", "FE518TW", new DateTime(2015, 10, 24), 19000, 
+        private static void ElencoMoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ElencoAuto()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ElencoVeicoli()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static char ScriviMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("*** GESTIONE RIVENDITA VEICOLI ***");
+            Console.WriteLine("1 - Visualizza TUTTI");
+            Console.WriteLine("2 - Visualizza AUTO");
+            Console.WriteLine("3 - Visualizza MOTO");
+            Console.WriteLine("\nQ - ESCI");
+            return Console.ReadKey(true).KeyChar;
+        }
+
+        private static void CreaDatiDiProva()
+        {
+            Veicolo v = new Auto("BMW", "Serie 3", "FE518TW", new DateTime(2015, 10, 24), 19000,
                 TipoAlimentazione.Benzina, 5, true);
-            Console.WriteLine(v);
+            ParcoMezzi.Add(v);
+            v = new Auto("Mercedes", "CLA", "GA331LD", new DateTime(2022, 2, 15), 38000,
+                TipoAlimentazione.Diesel, 5, false);
+            ParcoMezzi.Add(v);
 
-            Console.WriteLine();
-
-            v = new Moto("Ducati", "Diavolo Rosso", "EH654TY", new DateTime(2022, 7, 10), 8500, 
+            v = new Moto("Ducati", "Diavolo Rosso", "EH654TY", new DateTime(2022, 7, 10), 8500,
                 TipoMoto.Enduro, 4, false);
-            Console.WriteLine(v);
-
-            Console.WriteLine();
+            ParcoMezzi.Add(v);
 
             v = new Furgone("FIAT", "Ducato", "E451UF", new DateTime(2007, 3, 11), 4200, 12000);
-            Console.WriteLine(v);
-
-            Console.ReadLine();
+            ParcoMezzi.Add(v);
         }
     }
 }
