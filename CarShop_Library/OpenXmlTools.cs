8a4df23bb5b49e32f240751179c49ead79e0f13e
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-02-21 12:30:03 +0100

    A lot of formatting option for the run

--- CarShop_Library/OpenXmlTools.cs
+++ CarShop_Library/OpenXmlTools.cs
@@ -34,35 +34,52 @@ namespace CarShop_Library
                     Vivamus porttitor, nibh in maximus gravida, enim arcu porta leo, ac porttitor 
                     enim elit vel sapien.";
 
-                docBody.Append(CreaParagrafo(lorem, JustificationValues.Left));
-                docBody.Append(CreaParagrafo(lorem, JustificationValues.Center));
-                docBody.Append(CreaParagrafo(lorem, JustificationValues.Right));
+                // 3 paragrafi semplici con diversa giustificazione
+                docBody.Append(CreaParagrafo(JustificationValues.Left, lorem));
+                docBody.Append(CreaParagrafo(JustificationValues.Center, lorem));
+                docBody.Append(CreaParagrafo(JustificationValues.Right, lorem));
+
+                // un paragrafo con il contenuto formattato nei diversi run
+                Paragraph p = CreaParagrafo(JustificationValues.Center);
+                Run r = CreaRun("Testo normale"); p.Append(r);
+                r = CreaRun("Testo grassetto", true); p.Append(r);
+                r = CreaRun("Testo corsivo", false, true); p.Append(r);
+                r = CreaRun("Testo sottolineato", false, false, true); p.Append(r);
+                r = CreaRun("Testo grassetto, corsivo, sottolineato, colorato", true, true, true, "993300"); p.Append(r);
+                docBody.Append(p);
+
+                p = CreaParagrafo(JustificationValues.Left);
+                r = CreaRun("Testo con font arial 34", false, false, false, "000000", "Arial", 34);
+                p.Append(r);
+                docBody.Append(p);
             }
         }
 
-        public static Paragraph CreaParagrafo(string contenuto,
-            JustificationValues giustificazione)
+        public static Paragraph CreaParagrafo(JustificationValues giustificazione,
+            string contenuto = "")
         {
             Paragraph paragraph = new Paragraph();
             ParagraphProperties paragraphProperties = new ParagraphProperties();
             paragraphProperties.Justification = new Justification() { Val = giustificazione };
             paragraph.Append(paragraphProperties);
 
-            paragraph.Append(CreaRun(contenuto));
+            if (contenuto != "") paragraph.Append(CreaRun(contenuto));
             return paragraph;
         }
 
         public static Run CreaRun(string contenuto,
             bool isGrassetto = false, bool isCorsivo = false, bool isSottolineato = false,
-            string colore = "000000")
+            string colore = "000000", string fontFace = "Calibri", double fontSize = 11)
         {
             Run run = new Run();
             RunProperties runProperties = new RunProperties();
 
             if (isGrassetto) runProperties.Bold = new Bold();
             if (isCorsivo) runProperties.Italic = new Italic();
-            if (isSottolineato) runProperties.Underline = new Underline();
+            if (isSottolineato) runProperties.Underline = new Underline() { Val = UnderlineValues.Single };
             runProperties.Color = new Color() { Val = colore };
+            runProperties.RunFonts = new RunFonts() { Ascii = fontFace };
+            runProperties.FontSize = new FontSize() { Val=(fontSize*2).ToString() };
             run.Append(runProperties);
 
             Text text = new Text(contenuto);
